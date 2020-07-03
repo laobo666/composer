fbe9a4c071c5f7ed03c531579c7fb282cdd79019
laobo666 <915575770@qq.com>
2020-07-03 08:38:57 +0800

    test project

---
+++ application/web/controller/Test.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\web\controller;
+
+use think\Controller;
+
+class Test extends Controller
+{
+    public function test()
+    {
+        echo 'this is my test file';
+    }
+}
